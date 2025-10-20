661e3904e8094433a755c5dde5ff6feab2821f28
Deen Mohammad <deen9721@gmail.com>
2025-10-20 21:33:23 +0600

    Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom' // ← Import this
 import App from './App.tsx'
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
